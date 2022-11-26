6d864dac32a7ffc480c3cd5ee070a7cb6cd9c310
rhsgw <r-hsgw@hotmail.co.jp>
2022-11-26 15:33:59 +0900

    deserialize from object array

--- Program.cs
+++ Program.cs
@@ -10,22 +10,99 @@ Console.OutputEncoding = Encoding.GetEncoding(932);
 
 using (var stream = new MemoryStream())
 {
-	await JsonSerializer.SerializeAsync(stream, new Test(123, "にほんご日本語"));
-	Console.WriteLine(stream.GetString()); // {"Id":123,"Name":"\u306B\u307B\u3093\u3054\u65E5\u672C\u8A9E"} <- エスケープされてる
+	// 状況によって値の型が変わる配列を Deserialize したい
+	// (objectの配列としてJSONに Serialize => 本来の型を与えて Deserialize)
+	// RPC的な使い方を想定
+
+	var paramTypesArray = new[] { typeof(int), typeof(double), typeof(ulong), typeof(string), typeof(Test), typeof(Test2), typeof(Test3) };
+	await JsonSerializer.SerializeAsync(stream, new Request(123, "sample_method", 0, 1d, 2ul, "日本語", new Test(89, "internal"), new Test2(321, 654), new Test3(789, 456)));
+	Console.WriteLine(stream.GetString());
 	stream.Seek(0, SeekOrigin.Begin);
 
-	var doc = await JsonDocument.ParseAsync(stream);
-	var prop = doc.RootElement.EnumerateObject().FirstOrDefault(x => x.NameEquals("Name")); // prop は JsonProperty 構造体
-	Console.WriteLine(prop); // "Name":"\u306B\u307B\u3093\u3054\u65E5\u672C\u8A9E" <- 当然そのまま
+	Console.WriteLine();
 
-	using var output = new MemoryStream();
-	using var writer = new Utf8JsonWriter(output, new JsonWriterOptions() { Encoder = JavaScriptEncoder.Create(UnicodeRanges.All) }); // Encoder 指定
-	doc.WriteTo(writer); // この時点では writer で止まってる
-	await writer.FlushAsync(); // ここで stream に書き込み
+	// 書き出した JSON を JsonDocument に Parse する
+	var request = await JsonDocument.ParseAsync(stream);
+	var parameters = request.RootElement.EnumerateObject().FirstOrDefault(x => x.NameEquals("Parameters"));
+	Console.WriteLine($"Parameter's ValueKind : {parameters.Value.ValueKind}");
 
-	Console.WriteLine(output.GetString()); // {"Id":123,"Name":"にほんご日本語"}
+	Console.WriteLine();
+	Console.WriteLine("Parameter member's ValueKind");
+	foreach (var n in parameters.Value.EnumerateArray())
+		Console.WriteLine($"\t{n.ValueKind}");
+
+	Console.WriteLine();
+	Console.WriteLine("Deserialized");
+
+	var method = request.RootElement.EnumerateObject().FirstOrDefault(x => x.NameEquals("Method")).Value.GetString();
+	Console.WriteLine($"Method = {method}");
+
+	foreach (var n in parameters.Value.EnumerateArray().Zip(paramTypesArray,(v,t) => v.Deserialize(t)))
+		Console.WriteLine($"\t{n?.GetType()}, {n}");
 }
+
+/*
+{"Id":123,"Method":"sample_method","Parameters":[0,1,2,"\u65E5\u672C\u8A9E",{"Id":89,"Name":"internal"},{"P1":321,"P2":654},{"P1":789,"P2":456}]}
+
+Parameter's ValueKind : Array
+
+Parameter member's ValueKind
+        Number
+        Number
+        Number
+        String
+        Object
+        Object
+        Object
+
+Deserialized
+Method = sample_method
+        System.Int32, 0
+        System.Double, 1
+        System.UInt64, 2
+        System.String, 日本語
+        Test, Test { Id = 89, Name = internal }
+        Test2, P1:0,P2:0
+        Test3, P1:789,P2:456
+
+Test2 だけ Deserialize できてないのは readonly struct で、パラメータの書き換えができないから
+(Test3 みたいに readonly record struct にしちゃえばできる)
+ちなみに、日本語文字列はエスケープされた状態からちゃんと戻されて扱われてる
+(この JsonDocument をどこかに書き出すときには Encoder の指定が必要になる)
+*/
+
+// record class
 record Test(int Id, string Name);
+
+// readonly struct : Deserialize 不可
+readonly struct Test2
+{
+	public int P1 { get; }
+	public int P2 { get; }
+	public Test2(int p1, int p2) => (P1, P2) = (p1, p2);
+	public override string ToString() => $"P1:{P1},P2:{P2}";
+}
+
+// readonly record struct
+readonly record struct Test3(int P1, int P2)
+{
+	public override string ToString() => $"P1:{P1},P2:{P2}";
+}
+
+class Request
+{
+	public int Id { get; }
+	public string Method { get; }
+
+	public IReadOnlyCollection<object?> Parameters { get; }
+
+	public Request(int id, string method, params object?[] parameters)
+	{
+		Id = id;
+		Method = method;
+		Parameters = parameters;
+	}
+}
 
 static class StreamExtension
 {
